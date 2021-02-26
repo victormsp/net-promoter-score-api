bb74cb4e329bd083926bcf04f5d4dfbb4801c78b
Victor Peixoto <victormspeixoto@gmail.com>
2021-02-25 21:52:38 -0300

    Created Surveys migrations, models, repositories, controllers, routers.

--- src/routers.ts
+++ src/routers.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import { SurveryController } from './controllers/SurveyController';
 import { UserController } from './controllers/UserController';
 
 const router = Router();
 
 const userController = new UserController();
+const surveyController = new SurveryController();
 
 router.post('/users', userController.create);
+
+router.post('/surveys', surveyController.create);
+router.get('/surveys', surveyController.show);
 
 export { router };
