c75fd60e368c66aad089e88bac403eb3f74537e3
Victor Peixoto <victormspeixoto@gmail.com>
2021-02-26 23:19:23 -0300

    Created mail template.

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -1,9 +1,10 @@
-import { json, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { SurveysRepository } from '../repositories/SurveysRepository';
 import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
 import { UsersRepository } from '../repositories/UsersRepository';
 import SendMailService from '../services/SendMailService';
+import { resolve } from 'path';
 
 class SendMailController {
     async execute(request: Request, response: Response) {
@@ -25,6 +26,27 @@ class SendMailController {
             return response.status(400).json({ error: 'Survey does not exist!' });
         }
 
+        const variables = {
+            name: user.name,
+            title: survey.title,
+            description: survey.description,
+            id: '',
+            link: process.env.URL_MAIL,
+        };
+
+        const npsPath = resolve(__dirname, '..', 'views', 'emails', 'npsMail.hbs');
+
+        const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+            where: { user_id: user.id, value: null },
+            // relations: ['user', 'survey'],
+        });
+
+        if (surveyUserAlreadyExists) {
+            variables.id = surveyUserAlreadyExists.id;
+            await SendMailService.execute(email, survey.title, variables, npsPath);
+            return response.json(surveyUserAlreadyExists);
+        }
+
         const surveyUser = surveysUsersRepository.create({
             user_id: user.id,
             survey_id,
@@ -32,7 +54,9 @@ class SendMailController {
 
         await surveysUsersRepository.save(surveyUser);
 
-        await SendMailService.execute(email, survey.title, survey.description);
+        variables.id = surveyUser.id;
+
+        await SendMailService.execute(email, survey.title, variables, npsPath);
 
         return response.json(surveyUser);
     }
